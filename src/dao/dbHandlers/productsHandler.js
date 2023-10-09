ee59a4601b5eabdf1e50fae284f50f09fd5b134c
Fer <fmolachino@gmail.com>
2023-10-09 16:36:28 -0300

    product delete route +

--- src/dao/dbHandlers/productsHandler.js
+++ src/dao/dbHandlers/productsHandler.js
@@ -7,7 +7,7 @@ import { productsModel } from '../models/products.model.js';
 import path from 'path'
 //import __dirname from './utils.js'
 
-export class ProductManager{
+export class ProductManager {
 
     #productList = []
 
@@ -21,101 +21,109 @@ export class ProductManager{
 
     async getMaxIdFromDb() {
         try {
-          const result = await productsModel
-            .find()
-            .sort({ id: -1 })
-            .limit(1)
-            .exec();
-    
-          if (result.length > 0) {
-            return result[0].id;
-          } else {
-            return 0;
-          }
-        } catch (err) {
-          console.error(err);
-          return 0;
-        }
-      }
+            const result = await productsModel
+                .find()
+                .sort({ id: -1 })
+                .limit(1)
+                .exec();
 
-    async addProduct(title, description, price, thumbnail, code, stock, category){
-        if(!this.isCodeAvailable(code)){
-            throw new Error("The provided code: " +code+ " is already in use.");
-        } 
+            if (result.length > 0) {
+                return result[0].id;
+            } else {
+                return 0;
+            }
+        } catch (err) {
+            console.error(err);
+            return 0;
+        }
+    }
+
+    async addProduct(title, description, price, thumbnail, code, stock, category) {
+        if (!this.isCodeAvailable(code)) {
+            throw new Error("The provided code: " + code + " is already in use.");
+        }
         try {
-                let id = await this.getIdAndIncrease();
-                let product = await productsModel.create({
-                                                    id: id,
-                                                    title: title,
-                                                    description: description,
-                                                    price: price,
-                                                    thumbnail: thumbnail,
-                                                    code: code,
-                                                    stock: stock,
-                                                    category: category})
-                
-                console.log('Added product succesfully');
-                //checker TO delete
-                console.log("Added product: " + product.title);
-        } catch (err){
+            let id = await this.getIdAndIncrease();
+            let product = await productsModel.create({
+                id: id,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+                category: category
+            })
+
+            console.log('Added product succesfully');
+            //checker TO delete
+            console.log("Added product: " + product.title);
+        } catch (err) {
             console.error(err);
             //TODO
         }
     }
 
-    getProducts(){
+    getProducts() {
         return this.#productList
     }
 
-    getProductById(id){
-        
-        const product = this.#productList.find(product => product.id === id);
-
-        if (product) {
-            return product;
-        } else {
-            throw new Error("Product with id: " + id + " doesn't exist.");
-        }
-    }
-
-    updateProduct(id, toUpdateProduct){
-        const tempProd = this.getProductById(id)
-        const index = this.#productList.findIndex(product => product.id === id)
-
-        if(index !== -1) {
-            this.#productList[index] = toUpdateProduct;
-            this.saveToFile();
-        }
-        if(!this.isCodeAvailable(toUpdateProduct.code)){
-            throw new Error("The code: " + toUpdateProduct.code + " is already taken.")
-        } else {
-            throw new Error("Product with id: " + id + " doesn't exist.")
-        }
-    }
-
-    async updateProductFromPage(id, newProduct){
+    async getProductById(id) {
 
         try {
-            let oldProduct = await this.findOne({id: id})
+            let product = await productsModel.findOne({ id: id })
+            if (product) {
+                return product
+            }
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
+
+    async updateProductFromPage(id, newProduct) {
+
+        try {
+            let oldProduct = await productsModel.findOne({ id: id })
+            if (!oldProduct) {
+                console.log("The product with inputed id: " + id + " doesnt exists.");
+                return
+            }
+            let updatedProduct =
+                await productsModel.updateOne({
+                    title: newProduct.title,
+                    description: newProduct.description,
+                    price: newProduct.price,
+                    thumbnail: newProduct.thumbnail,
+                    code: newProduct.code,
+                    stock: newProduct.stock,
+                    category: newProduct.category,
+                    status: newProduct.status
+                })
+
         } catch (err) {
             console.error(err);
         }
-
     }
 
     //TODO control for non existent id
-    deleteProduct(id){
+    async deleteProduct(id) {
 
-        let prodIndex = this.getIndexFromId(id);
-
-        this.#productList.splice(prodIndex, 1);
-
-        //Rename id so if the last element is remove it can be used again
-        if((ProductManager.idProvider-1)===id){
-            ProductManager.setId(id);
+        try{
+            let productToDelete = await productsModel.findOne({id: id})
+            if(productToDelete){
+                let prodTitle = productToDelete.title;
+                await productsModel.deleteOne({id: id})
+                return prodTitle
+            }
+            else {
+                console.log(`The product with id: ${id}, doesnt exists.`);
+            }
         }
-
-        this.saveToFile();
+        catch (err) {
+            console.error(err);
+        }
 
     }
 
@@ -129,31 +137,31 @@ export class ProductManager{
 
         } catch (error) {
             console.error('Error checking product availability:', error);
-            return false; 
+            return false;
         }
     }
 
-    getIndexFromId(id){
+    getIndexFromId(id) {
         const index = this.#productList.findIndex(product => product.id === id)
         return index
     }
 
-    async getIdAndIncrease(){
+    async getIdAndIncrease() {
         try {
             const maxId = await this.getMaxIdFromDb();
-            
+
             const id = parseInt(maxId) + 1;
             return id;
-            }
-            
+        }
+
         catch (err) {
             console.error(err);
             return 0; // Return a default value or handle the error accordingly
         }
     }
 
-    
-    getThisProducts(){
+
+    getThisProducts() {
         return this.#productList
     }
 
@@ -162,19 +170,19 @@ export class ProductManager{
 
 
 
-export class Product{
+export class Product {
 
-    constructor(title, description, price, thumbnail, code, stock, id, category){
+    constructor(title, description, price, thumbnail, code, stock, id, category) {
 
-        this.title=title
-        this.description=description
-        this.price=price
-        this.thumbnail=thumbnail
-        this.code=code
-        this.stock=stock
-        this.category=category
-        this.id=id
-        this.status=true
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+        this.category = category
+        this.id = id
+        this.status = true
     }
 
 
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,14 +1,12 @@
 import {Router} from 'express';
 import path from 'path'
-import { testModel } from "../dao/models/test.model.js";
-import mongoose from "mongoose";
 
 export const router = Router();
 
 //import { products } from '../productsHandler.js';
 import __dirname from '../utils.js'
 import { ProductManager } from '../dao/dbHandlers/productsHandler.js';
-//import { Product } from '../dao/dbHandlers/productsHandler.js';
+import { Product } from '../dao/dbHandlers/productsHandler.js';
 
 let productsPath = path.join(__dirname, 'files', 'products.json');
 
@@ -44,7 +42,7 @@ router.get('/', (req,res)=>{
 
 })
 
-router.get('/:id', (req,res)=>{
+router.get('/:id',async (req,res)=>{
 
     let id = parseInt(req.params.id)
 
@@ -53,13 +51,12 @@ router.get('/:id', (req,res)=>{
         return
     }
 
-    let productById=products.filter(product=>product.id===id)
+    let productById= await productManager.getProductById(id)
 
-    if(productById.length>0){
+    if(productById){
         res.json({status:'ok', data:productById})
     } else {
         res.json({status: 'error', message: `Product with id: ${id} doesn't exists.`})
-        
     }
     
 
@@ -105,7 +102,7 @@ router.put('/:id', (req,res)=>{
 
 
 //Deleting an existing product
-router.delete('/:id', (req,res)=>{
+router.delete('/:id', async (req,res)=>{
 
     let id = parseInt(req.params.id)
 
@@ -114,35 +111,14 @@ router.delete('/:id', (req,res)=>{
         return
     }
 
-    let productToDeleteTitle = productManager.getProductById(id).title;
+    let productToDeleteTitle = await productManager.deleteProduct(id);
 
-    productManager.deleteProduct(id);
+    
 
     res.status(200).json({message:`The Product: ${productToDeleteTitle}, with id: ${id}, has been Deleted succesfully.`})
 
 })
 
-
-
-
-router.post('/test', async(req,res)=>{
-
-    let item = req.body;
-
-    if(!item)
-        return res.status(400).json({error:'Falta ingresar algun campo del producto'})
-
-    try {
-        let newItem=await testModel.create(item)
-    
-        res.status(201).json({newItem})
-        
-    } catch (error) {
-        res.status(500).json({error:'Error inesperado', detalle:error.message})
-        
-    }
-
-})
 
 
 
